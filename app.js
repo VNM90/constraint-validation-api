47c231b54ae0d94088e117303704dc3326f5c057
Arek <artek547@gmail.com>
2018-09-24 23:50:54 +0200

    validator

--- app.js
+++ app.js
@@ -7,9 +7,43 @@
     if (!this.fields.length) return;
 
     this.form.onsubmit = function(e) {
-      alert("herj");
-    };
-  }
+      e.preventDefault();
+      let formValid = this.validate();
 
-  var validator1 = new Validator(document.querySelector("#form"));
+      if (formValid) {
+        this.form.submit();
+      } else {
+        return false;
+      }
+    }.bind(this);
+  }
+  Validator.prototype.validate = function() {
+    for (let i = 0; i < this.fields.length; i++) {
+      this.validateField(this.field[i]);
+    }
+    if (!this.errors.length) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  Validator.prototype.validateField = function(field) {
+    let fieldValid = field.validity.valid;
+
+    if (fieldValid) {
+      this.markAsValid(field);
+    } else {
+      this.error.push(field.dateset.errorMessage);
+      this.markAsInvalid(field);
+    }
+  };
+  Validator.prototype.markAsValid = function(field) {
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+  };
+  Validator.prototype.markAsInvalid = function(field) {
+    field.classList.remove("valid");
+    field.classList.add("invalid");
+  };
+  let validator1 = new Validator(document.querySelector("#form"));
 })();
